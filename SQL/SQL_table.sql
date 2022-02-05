5178938e77fded6c6045d3a8e131c31c57efc472
MikeFQ <94359628+MikeFQ@users.noreply.github.com>
2022-02-05 19:35:11 +0300

    Update SQL_table.sql

--- SQL/SQL_table.sql
+++ SQL/SQL_table.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE candidates(	
-	id Serial  PRIMARY KEY,
+    id Serial  PRIMARY KEY,
     name varchar(50) NOT NULL,
     city varchar(50) NOT NULL,
     age int UNIQUE,
@@ -8,10 +8,10 @@
     
 INSERT INTO candidates(name, city, age, rating)
 	VALUES  ('Andy', 'Seattle', 41, 1892),
-			('Dough', 'NY', 36, 834),
-			('Joaquin', 'Orlando', 33, 2471),
-			('Edward', 'Dallas', 47, 859),
-			('Tyler', 'Vancouver', 39, 934);
+	        ('Dough', 'NY', 36, 834),
+		('Joaquin', 'Orlando', 33, 2471),
+		('Edward', 'Dallas', 47, 859),
+		('Tyler', 'Vancouver', 39, 934);
     
 SELECT name, city, rating FROM candidates
 WHERE rating >= 700 AND rating <= 2500
