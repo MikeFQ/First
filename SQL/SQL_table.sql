1c7d6bb954239f6de6ba4437c847b583c956ed51
MikeFQ <94359628+MikeFQ@users.noreply.github.com>
2022-02-06 17:07:35 +0300

    Update SQL_table.sql

--- SQL/SQL_table.sql
+++ SQL/SQL_table.sql
@@ -7,8 +7,8 @@
 )
     
 INSERT INTO candidates (name, city, age, rating)
-	VALUES  ('Andy', 'Seattle', 41, 1892),
-	        ('Dough', 'NY', 36, 834),
+	VALUES      ('Andy', 'Seattle', 41, 1892),
+	            ('Dough', 'NY', 36, 834),
 		    ('Joaquin', 'Orlando', 33, 2471),
 		    ('Edward', 'Dallas', 47, 859),
 		    ('Tyler', 'Vancouver', 39, 934);
