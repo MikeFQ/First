97c6023509fce3d8a6c57a2f23bd03c0e4f44b01
MikeFQ <94359628+MikeFQ@users.noreply.github.com>
2022-02-07 21:54:01 +0300

    Update SQL_table.sql

--- SQL/SQL_table.sql
+++ SQL/SQL_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE gamers (
             id Serial  PRIMARY KEY,
             name varchar(50) NOT NULL,
             city varchar(50) NOT NULL,
-            age int,
+            age int NOT NULL,
             rating int UNIQUE,
 	    platform varchar(50) NOT NULL
 )
@@ -11,7 +11,7 @@ INSERT INTO gamers (name, city, age, rating, platform)
 	VALUES      ('Andy', 'Seattle', 21, 9, 'PC'),
 	            ('Dough', 'NY', 36, 22, 'PS'),
 		    ('Joaquin', 'Orlando', 23, 7, 'PS'),
-		    ('Edward', 'Dallas', 17, 3, 'PC'),
+		    ('Edward', 'Dallas', 17, 20, 'PC'),
 		    ('Tyler', 'Vancouver', 39, 1, 'Xbox'),
 		    ('Alex', 'Moscow', 19, 2, 'PC'),
 		    ('Kate', 'London', 31, 4, 'Mobile'),
@@ -24,7 +24,10 @@ INSERT INTO gamers (name, city, age, rating, platform)
 		    ('Chu Hua', 'Beijing',28, 13, 'PC'),
 		    ('Dae', 'Seoul', 26, 14, 'Mobile'),
 		    ('Andrew', 'Adelaide', 21, 16, 'Xbox'),
-		    ('Ishaan', 'Mumbai', 31, 17, 'PC');
+		    ('Ishaan', 'Mumbai', 31, 17, 'PC'),
+		    ('Alexey', 'Bryansk', 28, 3, 'PC'),
+		    ('Maxim', 'Vologda', 34, 18, 'PC'),
+		    ('Sophia', 'Barcelona', 27, 19, 'PS');
     
 SELECT name, city, rating FROM gamers
 WHERE rating >= 3 AND rating <= 15
@@ -41,5 +44,10 @@ WHERE city ILIKE '%a%'
 
 SELECT COUNT(name) FROM gamers
 WHERE platform = 'PC'
+
+SELECT name, age FROM gamers
+WHERE platform = 'PS'
+
+
 
 
